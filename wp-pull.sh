a4f073073ddd7b40d30b9cc61da01b31cf61b890
Gavin Botica <gbotica@gmail.com>
2025-02-28 22:38:19 +1300

    start wp-pull script

--- wp-pull.sh
+++ wp-pull.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-script_version="1.3.0.1"
+script_version="1.0.0.0"
 # Author:        gb@wpnet.nz
-# Description:   Push / sync a site to another site, on the same server
+# Description:   Pull a site from another site, on the same server
 # Requirements: - This script has some security risks, USE WITH CAUTION!
 #               - NEVER permit an untrusted user to run this script, or to have CLI access while
 #                 the elevated permissions are in effect.
 #               - Requires a sudoers file at /etc/sudoers.d/ with permissions set accordingly
-#               - 'wp' must be available to the SOURCE and DESTINATION users
+#               - 'wp' must be available to the REMOTE and LOCAL users
 #               - Does not use SSH, adding to authorized_keys is not required
 #               - This script will NOT WORK unless appropriate permissions are enabled by an administrator beforehand
 # Known issues: - Won't work with sites that have the wp-config.php file above the webroot!
@@ -16,24 +16,24 @@ script_version="1.3.0.1"
 # EDIT FOLLOWING LINES ONLY!
 ####################################################################################
 
-# SOURCE
-source_user=""
-source_path=""           # use trailing slash
-source_webroot=""        # no preceding or trailing slash
+# LOCAL
+local_user=""
+local_path=""            # use trailing slash
+local_webroot=""         # no preceding or trailing slash
 
-# DESTINATION
-destination_user=""
-destination_path=""      # use trailing slash
-destination_webroot=""   # no preceding or trailing slash
+# REMOTE
+remote_user=""
+remote_path=""           # use trailing slash
+remote_webroot=""        # no preceding or trailing slash
 
 ####################################################################################
 # NO MORE EDITING BELOW THIS LINE!
 ####################################################################################
 
 # construct full paths
-source_full_path="${source_path}${source_webroot}"
-destination_full_path="${destination_path}${destination_webroot}"
-destination_original_domain=$(basename "$destination_path")
+remote_full_path="${remote_path}${remote_webroot}"
+local_full_path="${local_path}${local_webroot}"
+local_original_domain=$(basename "$local_path")
 
 # Options flags (don't change here, pass in arguments)
 exclude_wpconfig=1    # highly unlikely you want to change this
@@ -70,7 +70,8 @@ function status() {
 }
 
 # Set permissions for SOURCE user to access DESTINATION path (needed for find & wp)
-sudo /usr/bin/setfacl -m u:${source_user}:rwX ${destination_path}
+# SWAP?
+sudo /usr/bin/setfacl -m u:${remote_user}:rwX ${local_path}
 
 # Set DB dump filename, with random string and handle
 hash=$(echo $RANDOM | md5sum | head -c 12; echo;)
@@ -82,9 +83,9 @@ db_dump_sql="${db_export_prefix}${hash}${hash_handle}.sql"
 # Functions
 ####################################################################################
 
-# run commands as destination_user
-function sudo_as_dest_user() {
-    sudo -u $destination_user "$@"
+# run commands as local_user
+function sudo_as_remote_user() {
+    sudo -u $remote_user "$@"
 }
 
 # confirmation helper
@@ -105,19 +106,19 @@ function get_confirmation() {
 # (need to find a faster way to do this)
 function fetch_site_info() {
 
-    source_siteurl="$( wp option get siteurl --path=$source_full_path )"
-    source_blogname="$( wp option get blogname --path=$source_full_path )"
-    source_db_name="$( wp config get DB_NAME --path=$source_full_path )"
-    source_db_prefix="$(wp db prefix --path=$source_full_path )"
-    source_site_domain=${source_siteurl#http://}
-    source_site_domain=${source_siteurl#https://}
+    local_siteurl=$( wp option get siteurl --path=$local_full_path )
+    local_blogname=$( wp option get blogname --path=$local_full_path )
+    local_db_name="$( wp config get DB_NAME --path=$local_full_path )"
+    local_db_prefix=$( wp db prefix --path=$local_full_path )
+    local_site_domain=${local_siteurl#http://}
+    local_site_domain=${local_siteurl#https://}
 
-    destination_siteurl="$( sudo_as_dest_user wp option get siteurl --path=$destination_full_path )"
-    destination_blogname="$( sudo_as_dest_user wp option get blogname --path=$destination_full_path )"
-    destination_db_name="$( sudo_as_dest_user wp config get DB_NAME --path=$destination_full_path )"
-    destination_db_prefix="$( sudo_as_dest_user wp db prefix --path=$destination_full_path )"
-    destination_site_domain=${destination_siteurl#http://}
-    destination_site_domain=${destination_siteurl#https://}
+    remote_siteurl="$( sudo_as_remote_user wp option get siteurl --path=$remote_full_path )"
+    remote_blogname="$( sudo_as_remote_user wp option get blogname --path=$remote_full_path )"
+    remote_db_name="$( sudo_as_remote_user wp config get DB_NAME --path=$remote_full_path )"
+    remote_db_prefix=$( sudo_as_remote_user wp db prefix --path=$remote_full_path )
+    remote_site_domain=${remote_siteurl#http://}
+    remote_site_domain=${remote_siteurl#https://
 
 }
 
@@ -125,16 +126,16 @@ function fetch_site_info() {
 function print_summary() {
 cat <<EOF
 SUMMARY:
-    SOURCE:
-        blogname @ siteurl:  ${source_blogname} @ ${source_siteurl}
-        user @ path:         ${source_user} @ ${source_full_path}
-        database name:       ${source_db_name}
-        table_prefix:        ${source_db_prefix}
-    DESTINATION:
-        blogname @ siteurl:  ${destination_blogname} @ ${destination_siteurl}
-        user @ path:         ${destination_user} @ ${destination_full_path}
-        database name:       ${destination_db_name}
-        table_prefix:        ${destination_db_prefix}
+    LOCAL:
+        blogname @ siteurl:  ${local_blogname} @ ${local_siteurl}
+        user @ path:         ${local_user} @ ${local_full_path}
+        database name:       ${local_db_name}
+        table_prefix:        ${local_db_prefix}
+    REMOTE:
+        blogname @ siteurl:  ${remote_blogname} @ ${remote_siteurl}
+        user @ path:         ${remote_user} @ ${remote_full_path}
+        database name:       ${remote_db_name}
+        table_prefix:        ${remote_db_prefix}
 EOF
     echo "    OPTIONS:"
     (( files_only == 1)) && echo "      Files only: ${files_only}"
@@ -172,17 +173,17 @@ function set_verbose() {
 tidy_up_db_dumps() {
 
     status "TIDY UP: Search and delete database dump files ..."
-    status "Found in SOURCE:"
-    find "${source_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -print
-    status "Found in DESTINATION:"
-    find "${destination_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -print
+    status "Found in LOCAL:"
+    find "${local_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -print
+    status "Found in REMOTE:"
+    find "${remote_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -print
 
     if $(get_confirmation "DELETE any found database dump files?"); then
         status "Deleting database dump files ..."
-        # SOURCE
-        find "${source_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -delete
-        # DESTINATION
-        find "${destination_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -delete
+        # LOCAL
+        find "${local_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -delete
+        # REMOTE
+        find "${remote_path}" -maxdepth 1 -name "${db_export_prefix}*${hash_handle}.sql" -delete
         status "Done!"
     else
         status "ABORTED!"
@@ -240,14 +241,14 @@ fi
 fetch_site_info
 
 # START output
-status "PUSH site from '${source_full_path}' to '${destination_full_path}'"
+status "PULL site FROM '${remote_full_path}' TO '${local_full_path}'"
 (( be_verbose == 1 )) && echo "Script: $0 v${script_version}"
 
 # Print SUMMARY
 (( be_verbose == 1 )) && print_summary
 
 # CONFIRM before proceeding
-if ( ! get_confirmation "Proceed with PUSH?" ); then
+if ( ! get_confirmation "Proceed with PULL?" ); then
     status "ABORTED!"
     exit
 fi
@@ -256,17 +257,17 @@ fi
 # CHECK WP table_prefixes match, if not reset DESTINATION database
 ####################################################################################
 
-if [[ $source_db_prefix != "$destination_db_prefix" ]] && (( files_only == 0 )); then
-    status "ERROR: SOURCE and DESTINATION database prefixes do not match!"
-    status "To proceed, the DESTINATION database will be RESET and the \$table_prefix in wp-config.php will be updated."
-    if ( get_confirmation "Reset DESTINATION site's database?" ) ; then
-        if ( get_confirmation "WARNING! This will DELETE ALL tables in database: '${destination_db_name}'! Not just those with table prefix '${destination_db_prefix}'!" ) ; then
+if [[ "$local_db_prefix" != $remote_db_prefix ]] && (( files_only == 0 )); then
+    status "ERROR: LOCAL and REMOTE database prefixes do not match!"
+    status "To proceed, the LOCAL database will be RESET and the \$table_prefix in wp-config.php will be updated to match the REMOTE."
+    if ( get_confirmation "Reset LOCAL site's database?" ) ; then
+        if ( get_confirmation "WARNING! This will DELETE ALL tables in database: '${local_db_name}'! Not just those with table prefix '${local_db_prefix}'!" ) ; then
             status "Resetting DESTINATION database ..."
-            sudo_as_dest_user wp db reset --yes --path=$destination_full_path
+            wp db reset --yes --path=$local_full_path
             # ALTERNATIVE: DROP only tables with the current table_prefix
-            # sudo_as_dest_user wp db clean --yes --path=$destination_full_path 
-            status -n "Updating the \$table_prefix in the DESTINATION wp-config.php file ..."
-            sudo_as_dest_user wp config set table_prefix "${source_db_prefix}" --path=$destination_full_path
+            # wp db clean --yes --path=$local_full_path 
+            status -n "Updating the \$table_prefix in the LOCAL wp-config.php file ..."
+            wp config set table_prefix "${remote_db_prefix}" --path=$local_full_path
         else
             status "ABORTED!" && exit
         fi
@@ -280,26 +281,28 @@ fi
 ####################################################################################
 
 if (( db_only == 0 )); then
-    status "RSYNC files to DESTINATION: ${destination_full_path} ..."
-    echo "++++ NOTE: Any files at DESTINATION not present in SOURCE will be DELETED!"
+    status "RSYNC files to LOCAL: ${local_full_path} ..."
+    echo "++++ NOTE: Any files at LOCAL not present in REMOTE will be DELETED!"
     echo "++++ EXCLUSIONS: ${excludes[@]}"
     (( be_verbose == 1 )) && quiet="" || quiet="--quiet"
-    sudo rsync ${quiet} -azhP --delete --chown=${destination_user}:${destination_user} $(printf -- "--exclude=%q " "${excludes[@]}") ${source_full_path}/ ${destination_full_path} # slash after source_full_path is IMPORTANT!
+    sudo rsync ${quiet} -azhP --delete --chown=${local_user}:${local_user} $(printf -- "--exclude=%q " "${excludes[@]}") ${remote_full_path}/ ${local_full_path} # slash after remote_full_path is IMPORTANT!
 fi
+
+# PICKUP FROM HERE
 
 ####################################################################################
 # DUMP the SOURCE database
 ####################################################################################
 
 if (( files_only == 0 )); then
-    status "EXPORT database ... (${source_db_name})"
-    wp db export ${source_path}${db_dump_sql} --path=$source_full_path
+    status "EXPORT database ... (${remote_db_name})"
+    wp db export ${remote_path}${db_dump_sql} --path=$remote_full_path
     # RSYNC database dump to DESTINATION
     (( be_verbose == 1 )) && status "COPY database to DESTINATION ..."
-    if sudo rsync --quiet -azhP --chown=${destination_user}:${destination_user} ${source_path}${db_dump_sql} ${destination_path}; then
+    if sudo rsync --quiet -azhP --chown=${local_user}:${local_user} ${remote_path}${db_dump_sql} ${local_path}; then
         status "SUCCESS Database copied to DESTINATION!"
         (( be_verbose == 1 )) && status "Delete database dump source file ..."
-        rm ${verbose} ${source_path}${db_dump_sql}
+        rm ${verbose} ${remote_path}${db_dump_sql}
     else
         status "ERROR: Database copy to DESTINATION failed!"
         exit
@@ -313,15 +316,15 @@ fi
 if (( files_only == 0 && no_db_import == 0 )); then
     if ( get_confirmation "Proceed with IMPORT?"); then
         status "IMPORT database to DESTINATION ..."
-        sudo_as_dest_user wp db import ${destination_path}${db_dump_sql} --path=$destination_full_path
+        sudo_as_remote_user wp db import ${local_path}${db_dump_sql} --path=$local_full_path
         status "DB IMPORT COMPLETE!"
-        sudo_as_dest_user wp cache flush --hard --path=$destination_full_path
+        sudo_as_remote_user wp cache flush --hard --path=$local_full_path
         if (( be_verbose == 1 )); then
             echo -n "New (TEMPORARY!) DESTINATION siteurl:  "
-            sudo_as_dest_user wp option get siteurl --path=$destination_full_path
+            sudo_as_remote_user wp option get siteurl --path=$local_full_path
         fi
         (( be_verbose == 1 )) && status "DELETE imported database source file ..."
-        sudo_as_dest_user find "${destination_path}" -name "${db_dump_sql}" -delete 2>/dev/null
+        sudo_as_remote_user find "${local_path}" -name "${db_dump_sql}" -delete 2>/dev/null
     else
         do_search_replace=0
     fi
@@ -338,20 +341,20 @@ if (( files_only == 0 && no_db_import == 0 )); then
                 newline="-n"; format="count"
             fi
             echo -e ${newline} "$lh EXECUTE 'wp search-replace' for URLs ... changed:${clr_reset} "
-            sudo_as_dest_user wp search-replace --precise //${source_site_domain} //${destination_original_domain} --path=$destination_full_path --report-changed-only --format=${format}
+            sudo_as_remote_user wp search-replace --precise //${remote_site_domain} //${local_original_domain} --path=$local_full_path --report-changed-only --format=${format}
             echo -e ${newline} "$lh EXECUTE 'wp search-replace' for file PATHs ... changed:${clr_reset} "
-            sudo_as_dest_user wp search-replace --precise ${source_full_path} ${destination_full_path} --path=$destination_full_path --report-changed-only --format=${format}
-            sudo_as_dest_user wp cache flush --hard --path=$destination_full_path
+            sudo_as_remote_user wp search-replace --precise ${remote_full_path} ${local_full_path} --path=$local_full_path --report-changed-only --format=${format}
+            sudo_as_remote_user wp cache flush --hard --path=$local_full_path
             echo -ne "${clr_yellow}NEW${clr_reset} DESTINATION blogname: "
-            sudo_as_dest_user wp option get blogname --path=$destination_full_path
+            sudo_as_remote_user wp option get blogname --path=$local_full_path
             echo -ne "${clr_yellow}NEW${clr_reset} DESTINATION siteurl:  "
-            sudo_as_dest_user wp option get siteurl --path=$destination_full_path
+            sudo_as_remote_user wp option get siteurl --path=$local_full_path
         fi
     fi
 
 fi
 
 # REMOVE the additional permissions from the SOURCE user
-sudo /usr/bin/setfacl -x u:${source_user} ${destination_path}
+sudo /usr/bin/setfacl -x u:${remote_user} ${local_path}
 status "PUSH completed!"
 exit
